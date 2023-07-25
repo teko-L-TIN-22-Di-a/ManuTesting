1e9a5dee62b2e5c64780d756948a56fc28e00238
Yillie <tobia.schaerli@hotmail.com>
2023-07-25 20:30:32 +0200

    Bugfixes AI

--- src/TicTacToeee.java
+++ src/TicTacToeee.java
@@ -5,134 +5,132 @@ import java.util.Random;
 import java.util.Scanner;
 
 
-
 public class TicTacToeee {
 
     static ArrayList<Integer> playerPositions = new ArrayList<Integer>();
     static ArrayList<Integer> cpuPositions = new ArrayList<Integer>();
-   
+
     public static void main(String[] args) {
-      
-        char [][] gameBoard ={{' ', '|', ' ', '|', ' '},
-                              {'-', '+', '-', '+', '-'},
-                              {' ', '|', ' ', '|', ' '},
-                              {'-', '+', '-', '+', '-'},
-                              {' ', '|', ' ', '|', ' '},};
-       
-        
-        
-        
-        while(true){
-            
+
+        char[][] gameBoard = {{' ', '|', ' ', '|', ' '},
+                {'-', '+', '-', '+', '-'},
+                {' ', '|', ' ', '|', ' '},
+                {'-', '+', '-', '+', '-'},
+                {' ', '|', ' ', '|', ' '},};
+
+
+        while (true) {
+
             Scanner scan = new Scanner(System.in);
             System.out.println("Enter your number 1-9:");
             int playerPos = scan.nextInt();
-            while(playerPositions.contains(playerPos) || cpuPositions.contains(playerPos)) {
+            while (playerPositions.contains(playerPos) || cpuPositions.contains(playerPos)) {
                 System.out.println("Position taken. Try agian.");
                 playerPos = scan.nextInt();
             }
-            
-             placePiece(gameBoard, playerPos, "player");
-             
-             String result = checkWinner();
-              if (result.length() > 0) {
-               System.out.println(result);
-               break;
-             }
-        
+
+            placePiece(gameBoard, playerPos, "player");
+
+            String result = checkWinner();
+            if (result.length() > 0) {
+                printGameBoard(gameBoard);
+                System.out.println(result);
+                break;
+            }
+
             Random rand = new Random();
-            int cpuPos = rand.nextInt(9) +1;
-             while(playerPositions.contains(cpuPos) || cpuPositions.contains(cpuPos)) {
-                cpuPos = rand.nextInt(9) +1;
+            int cpuPos = rand.nextInt(9) + 1;
+            while (playerPositions.contains(cpuPos) || cpuPositions.contains(cpuPos)) {
+                cpuPos = rand.nextInt(9) + 1;
             }
             placePiece(gameBoard, cpuPos, "cpu");
 
-             printGameBoard(gameBoard);
-             
-             result = checkWinner();
-             if (result.length() > 0) {
-               System.out.println(result);
-               break;
-             }
-           
-             
-        }
-    }
-    
-    public static void printGameBoard(char [][] gameBoard){
-        for(char[] row: gameBoard) {
-            for(char c : row){
-            System.out.print(c);
+            printGameBoard(gameBoard);
+
+            result = checkWinner();
+            if (result.length() > 0) {
+                System.out.println(result);
+                break;
             }
-        System.out.println();
+
+
         }
-        
     }
-    
+
+    public static void printGameBoard(char[][] gameBoard) {
+        for (char[] row : gameBoard) {
+            for (char c : row) {
+                System.out.print(c);
+            }
+            System.out.println();
+        }
+
+    }
+
     public static void placePiece(char[][] gameBoard, int pos, String user) {
-    
+
         char symbol = ' ';
-        
-        if(user.equals("player")) {
+
+        if (user.equals("player")) {
             symbol = 'X';
-            
-            
-        } else if(user.equals("cpu")) {
-            symbol = 'O'; 
+            playerPositions.add(pos);
+
+        } else if (user.equals("cpu")) {
+            symbol = 'O';
             cpuPositions.add(pos);
-            
+
         }
-        
-         
-        switch(pos) {
+
+
+        switch (pos) {
             case 1:
-            gameBoard [0][0] = symbol;
-            break;
-            
+                gameBoard[0][0] = symbol;
+                break;
+
             case 2:
-            gameBoard [0][2] = symbol;
-            break;
-            
+                gameBoard[0][2] = symbol;
+                break;
+
             case 3:
-            gameBoard [0][4] = symbol;
-            break;
-            
+                gameBoard[0][4] = symbol;
+                break;
+
             case 4:
-            gameBoard [2][0] = symbol;
-            break;
-            
+                gameBoard[2][0] = symbol;
+                break;
+
             case 5:
-            gameBoard [2][2] = symbol;
-            break;
-            
+                gameBoard[2][2] = symbol;
+                break;
+
             case 6:
-            gameBoard [2][4] = symbol;
-            break;
-            
+                gameBoard[2][4] = symbol;
+                break;
+
             case 7:
-            gameBoard [4][0] = symbol;
-            break;
-            
+                gameBoard[4][0] = symbol;
+                break;
+
             case 8:
-            gameBoard [4][2] = symbol;
-            break;
-            
+                gameBoard[4][2] = symbol;
+                break;
+
             case 9:
-            gameBoard [4][4] = symbol;
-            break;
+                gameBoard[4][4] = symbol;
+                break;
         }
     }
-    
-    public static String checkWinner(){
-        List topRow = Arrays.asList (1, 2, 3);
-        List midRow = Arrays.asList (4, 5, 6);
-        List botRow = Arrays.asList (7, 8, 9);
-        List leftCol = Arrays.asList (1, 4, 7);
-        List midCol = Arrays.asList (2, 5, 8);
-        List rightCol = Arrays.asList (3, 6, 9);
-        List cross1 = Arrays.asList (1, 5, 9);
-        List cross2 = Arrays.asList (3, 5, 7);
-        
+
+    public static String checkWinner() {
+        List topRow = Arrays.asList(1, 2, 3);
+        List midRow = Arrays.asList(4, 5, 6);
+        List botRow = Arrays.asList(7, 8, 9);
+        List leftCol = Arrays.asList(1, 4, 7);
+        List midCol = Arrays.asList(2, 5, 8);
+        List rightCol = Arrays.asList(3, 6, 9);
+        List cross1 = Arrays.asList(1, 5, 9);
+        List cross2 = Arrays.asList(3, 5, 7);
+
         List<List> winning = new ArrayList<List>();
         winning.add(topRow);
         winning.add(midRow);
@@ -142,18 +140,18 @@ public class TicTacToeee {
         winning.add(rightCol);
         winning.add(cross1);
         winning.add(cross2);
-        
+
         for (List l : winning) {
             if (playerPositions.containsAll(l)) {
                 return "Congratulations you won!";
             } else if (cpuPositions.containsAll(l)) {
                 return "CPU wins. Try again.";
-            } else if (playerPositions.size() + cpuPositions.size() ==9) {
-                return "Tie! Try again."; 
+            } else if (playerPositions.size() + cpuPositions.size() == 9) {
+                return "Tie! Try again.";
             }
         }
-        
-        
-        return"";
-    }  
+
+
+        return "";
+    }
 }
